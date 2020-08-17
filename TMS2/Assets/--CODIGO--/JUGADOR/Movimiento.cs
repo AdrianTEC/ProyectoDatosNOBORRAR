dba33352b40318041448e8d1a7e70e2553d4cd18
Adrian <adriantec2019@gmail.com>
2020-08-16 18:16:12 -0600

    Movimiento del personaje por medio de fuerza
    Se agregó un modelo de pato
    Se agregó un script que permite mover al pato con fuerzas
    -se puede girar la cámara

---
+++ TMS2/Assets/--CODIGO--/JUGADOR/Movimiento.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{	
+	private Rigidbody rb;
+	private  float audioSpeed=1f;//
+ 	private AudioSource Audio;
+    private float constanteFuerza=0.5f;
+    private float constanteRotacion=400;
+    private int  constanteVelocidadMaxima=5;
+
+    private string ultimaTeclaPresionada;
+	//Reproduce un sonido
+
+	public void Playthesound()
+		{
+            
+			Audio.pitch=audioSpeed*0.8f;
+	   		if (true){if (!Audio.isPlaying){Audio.Play();}}
+	   		else{Audio.Stop();}
+
+	    }
+
+    void Start()
+		{
+			rb= gameObject.GetComponent<Rigidbody>();
+			Audio= GetComponent<AudioSource>();
+			
+		}
+
+    void Update()
+		{
+
+            // Debug.Log("me muevo a " + rb.velocity);
+			if (Input.GetKey("w"))
+				{
+					rb.AddForce(transform.forward*constanteFuerza, ForceMode.VelocityChange);
+				}
+            if(Input.GetKey("a"))
+				{
+				    rb.AddForce(transform.right*-constanteFuerza, ForceMode.VelocityChange);
+                    rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, -constanteRotacion, 0) * Time.deltaTime));
+				}  
+            if(Input.GetKey("s"))
+				{
+					rb.AddForce(-transform.forward*constanteFuerza , ForceMode.VelocityChange);
+				}      
+            if(Input.GetKey("d"))
+				{
+					rb.AddForce(transform.right*constanteFuerza, ForceMode.VelocityChange);
+                    rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, constanteRotacion, 0) * Time.deltaTime));
+				}
+
+
+			if(Input.GetKeyDown("space"))
+                {
+                  rb.AddForce(new Vector3(0,5,0) , ForceMode.VelocityChange);
+                }
+
+
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, constanteVelocidadMaxima);
+
+
+
+		}
+}
---
+++ TMS2/Assets/--CODIGO--/JUGADOR/RotacionCamara.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionCamara : MonoBehaviour
+{   
+    private GameObject myGameObj;
+    private int speed=2;
+    void Start()
+        {
+            myGameObj= GameObject.Find("PATO");
+        }
+    void Update()
+    {   
+            if(Input.GetMouseButton(1)){
+                this.transform.RotateAround(myGameObj.transform.position, this.transform.up,-Input.GetAxis("Mouse X")*speed);
+            }
+    }
+}
