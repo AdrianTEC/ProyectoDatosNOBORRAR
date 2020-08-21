29c82d9a0c47540c114586d64dca1b3d7bee3ca9
Adrian <adriantec2019@gmail.com>
2020-08-21 11:29:29 -0600

    Menu de items, animacion cuerpo, clase pickleable, canva

--- TMS2/Assets/--CODIGO--/ARTEFACTOS/Pickable.cs
+++ TMS2/Assets/--CODIGO--/ARTEFACTOS/Pickable.cs
@@ -1,31 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pickable : MonoBehaviour
 {
 
     public  string tipo;
+
+    public string nombre;
+    public string description;
     public  Vector3 localPosition;
     public Vector3 localRotation;
     private Rigidbody rb;
     private BoxCollider boxCollider;
     public string lugar;
+    public Sprite icon;
+    public bool picked;
+
+    //tipos pueden ser
+    // sw sh l k i
+    // espada escudo lanza llave item
     void Start()
         {
+
+            picked=false;
+
             rb  =gameObject.GetComponent<Rigidbody>();
             boxCollider=gameObject.GetComponent<BoxCollider>();
-
+            
         }
     void OnCollisionEnter(Collision other)
         {
-                if(other.gameObject.tag=="Player")
+        
+                if(other.gameObject.tag=="Player" &&!picked)
                     {
-
+                        picked=true;
                         other.gameObject.GetComponent<Equipamiento>().EquiparObjeto(gameObject,localPosition,localRotation,lugar);
+
+                        Inventario inventario= other.gameObject.GetComponent<Inventario>();
+                        inventario.add(this.gameObject);
+                        inventario.mostrarLista();
+
                         rb.useGravity=false;
                         rb.isKinematic=true;
-                        if(tipo.Equals("s")|| tipo.Equals("i"))
+                        if(tipo.Equals("sh")|| tipo.Equals("i"))
                         {
                             boxCollider.enabled=false;
                         }
---
+++ TMS2/Assets/--CODIGO--/JUGADOR/Inventario.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Inventario : MonoBehaviour
+{
+    public List<GameObject> ArmasyHerramientas= new List<GameObject>();
+    public List<GameObject> items= new List<GameObject>(); 
+    public List<GameObject> coleccionables= new List<GameObject>(); 
+    public bool inventarioAbierto=false;
+    public GameObject canva;
+    public GameObject item;
+    private GameObject contenido;
+    private GameObject menu;
+
+
+    private void Start() 
+        {
+                
+        }
+    public void mostrarLista()
+        {
+            foreach( GameObject x in ArmasyHerramientas)
+            {
+                Debug.Log(x); 
+            }
+        }
+    public void add(GameObject thing)
+        {
+            ArmasyHerramientas.Add(thing);
+        }
+
+    private void Update() 
+        {
+		    if(Input.GetKeyDown(KeyCode.Escape))
+                {
+                        if(!inventarioAbierto)
+                            {
+                                    abrirInventario();
+                                    inventarioAbierto=true;
+                            }
+                        else
+                            {
+                                    cerrarInventario();
+                                    inventarioAbierto=false;
+                            }
+                }
+        }
+
+    private void abrirInventario()
+        {
+            menu =Instantiate(canva);
+            contenido= menu.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+
+            foreach(GameObject thing in ArmasyHerramientas)
+                {
+
+                    GameObject por_agregar = Instantiate(item);
+
+                    Pickable data=thing.GetComponent<Pickable>();
+                    por_agregar.transform.GetChild(0).gameObject.GetComponent<Text>().text= data.nombre;
+                    por_agregar.transform.GetChild(1).gameObject.GetComponent<Image>().sprite=data.icon;
+                    por_agregar.transform.SetParent(contenido.transform);
+
+
+                }
+            Time.timeScale = 0.0f;
+        }
+    private void cerrarInventario()
+        {   
+            Destroy(menu);
+            Time.timeScale = 1.0f;
+        }
+}
