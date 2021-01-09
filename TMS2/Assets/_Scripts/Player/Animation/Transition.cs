2996df8814dfde6bd856e8105da25db7feeea8cc
Adrián <adriantec2019@gmail.com>
2021-01-08 22:11:22 -0600

    Corrección Animaciones

--- TMS2/Assets/_Scripts/Player/Animation/Transition.cs
+++ TMS2/Assets/_Scripts/Player/Animation/Transition.cs
@@ -1,53 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Transition : StateMachineBehaviour
 {
     public bool confirmedNext;
-    public string parameter;
-    private int value;
-    public int setValue;
+
+    public List<AnimationParameter< bool>> Booleans;
+    public List<AnimationParameter< int>> Integers;
+    public List<AnimationParameter< float>> Floats;
     
     
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        confirmedNext = false;
-        value = animator.GetInteger(parameter);
-        animator.SetInteger(parameter, 0);
 
-    }
-
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public void setValues(Animator animator,bool reset)
     {
-        if (Input.GetMouseButtonDown(0))
+        if(!reset)
         {
-            if( !confirmedNext)
-                animator.SetInteger(parameter, setValue);
-            confirmedNext = true;
+            foreach (var param in Booleans)
+                animator.SetBool(param.Parameter, param.Value);
+            foreach (var param in Integers)
+                animator.SetInteger(param.Parameter, param.Value);
+            foreach (var param in Floats)
+                animator.SetFloat(param.Parameter, param.Value);
         }
-     
-        
-    }
+        else
+        {
+            foreach (var param in Booleans)
+                animator.SetBool(param.Parameter, param.DefaultValue);
+            foreach (var param in Integers)
+                animator.SetInteger(param.Parameter, param.DefaultValue);
+            foreach (var param in Floats)
+                animator.SetFloat(param.Parameter, param.DefaultValue);
+        }
+    } 
     
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        
-        if(!confirmedNext)
-            animator.SetInteger("Attack", 0);
-        confirmedNext = false;
-    }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    
+}
+[Serializable]
+public struct AnimationParameter<T> 
+{
+    public string Parameter;
+    public T Value;
+    public T DefaultValue;
 }
---
+++ TMS2/Assets/_Scripts/Player/FightBehavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FightBehavior : MonoBehaviour
+{
+    public Animator _anim;
+    private Equipment _equipment;
+    private static readonly int Attack = Animator.StringToHash("Attack");
+
+
+    void Start()
+    {
+        _equipment = GetComponent<Equipment>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0)&& _anim.GetInteger(Attack)==0)
+        {
+            _anim.SetInteger(Attack,1);
+            _anim.Play(Attack);
+        }
+    }
+}
